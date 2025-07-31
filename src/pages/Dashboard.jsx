f19a6cb6d862fc3cd56cfd96c1b044637def1fa9
Yogihafidh <hafidhyogi@gmail.com>
2025-07-31 23:18:59 +0700

    feat: add button liat semua for see all kendaraan  disewa

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,24 @@
+import { useNavigate } from "react-router";
 import DashboardSumarry from "../features/dashboard/DashboardSumarry";
 import KendaraanCard from "../features/dashboard/KendaraanCard";
 
 function Dashboard() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/rental?status=Disewa");
+  };
+
   return (
     <div className="flex flex-col gap-10">
       <DashboardSumarry />
       <div>
         <div className="mb-4 flex items-center justify-between">
           <p>Kendaraan Disewa</p>
-          <p className="hover:text-acent-blue cursor-pointer hover:underline">
+          <p
+            onClick={handleClick}
+            className="hover:text-acent-blue cursor-pointer hover:underline"
+          >
             Lihat Semua
           </p>
         </div>
